5674942b6602db563f00c2df23c8dd49156b97e4
Giulia Martinelli <giulia.martinelli.uni@gmail.com>
2020-10-26 19:58:27 +0100

    implemented navbar, aborted flashes, created seed for every model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email:"giulia@gmail.com", password:"hellosoraya123", name:"giulia", is_mentor:true)
+User.create(email:"marco@notgmail.com", password:"hellonaelle123", name:"marco", is_mentor:false)
+Category.create(name:"public")
+Lesson.create(user_id:1, name:"speaking in public", description:"we will talk", duration:1, category_id:1)
+Booking.create(date: Date.today, lesson_id:1, user_id:2)
+
