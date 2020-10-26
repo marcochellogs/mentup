08d2ab95488f8b966a611bf0b7ef03ebb43b5700
marco checchi <marco.checchi2@gmail.com>
2020-10-26 18:29:09 +0100

    all routes, controllers and views are ready

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,8 +1,10 @@
 class Lesson < ApplicationRecord
   belongs_to :category
   belongs_to :user
-  has_many :bookings
+
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
+  has_many :reviews, through: :bookings
 
   # validates :name, :description, :price, :duration, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :lessons
+  resources :lessons do
+    resources :bookings, only: [:new, :create]
+    resources :reviews, only: [:index]
+  end
+  resources :bookings, only: [:index, :show, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end
+  get '/profile', to: 'pages#profile', as: 'profile'
   # new controller profile for the
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
