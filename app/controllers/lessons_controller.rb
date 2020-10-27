7042f91468981dd8226e323a02603739af7ae27a
Giulia Martinelli <giulia.martinelli.uni@gmail.com>
2020-10-27 15:02:08 +0100

    devise autchentication skipped also for lesson#show

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,5 @@
 class LessonsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_lesson, only: [:show, :edit, :update, :destroy]
 
   def index
