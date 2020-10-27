0b74e484d78e73f86c97a1fcd23d1244bd380e7b
Giulia Martinelli <giulia.martinelli.uni@gmail.com>
2020-10-27 20:42:25 +0100

    resolved reviews issues in the model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,8 +5,8 @@ class Lesson < ApplicationRecord
 
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
-  has_many :reviews, through: :bookings
-  has_many :reviews, dependent: :destroy
+  has_many :reviews, through: :bookings, dependent: :destroy
+
 
   has_one_attached :photo # cloudinary lesson pic for card
 
