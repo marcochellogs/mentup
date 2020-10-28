f7fe92d5d5f19641b07dd99a27b7b4c8819271e1
Giulia Martinelli <giulia.martinelli.uni@gmail.com>
2020-10-28 11:14:40 +0100

    pull again

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,11 +9,11 @@ class PagesController < ApplicationController
     # @reviews = current_user.reviews #non piace a giulia
     @lessons = current_user.lessons # lessons booked as a user
     if current_user.is_mentor
-      @mentor_lesson = current_user.lesson
-      @mentor_bookings = @mentor_lesson ? @mentor_lesson.bookings : current_user.bookings
+      @mentor_lesson = current_user.lesson # your lesson as a mentor
+      if @mentor_bookings != nil
+        @mentor_bookings = @mentor_lesson.bookings # mentor bookings of your lesson
+      end
     end
-  end
 end
-
-# User has one picture to upload as profile pic
+end
 
