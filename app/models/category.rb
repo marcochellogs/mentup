f46b1f0a01d16146d77495e8febc05166169ed79
Naelle Burger <naelle.burger@gmx.at>
2020-10-28 14:48:19 +0100

    category images are working

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_many :lessons
+  has_one_attached :image
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # Cloudinary::Uploader.upload("san_francisco.jpg")
-User.create(email:"giulia@gmail.com", password:"hellosoraya123", name:"giulia", bio:"Hello world", is_mentor:true)
-User.create(email:"marco@notgmail.com", password:"hellonaelle123", name:"marco", bio:"Hello world", is_mentor:false)
-Category.create(name:"public")
-Category.create(name:"cooking")
-Lesson.create(user_id:1, name:"speaking in public", description:"we will talk", duration:1, price:"40", category_id:1)
-Booking.create(date: Date.today, lesson_id:1, user_id:2)
-Review.create(title:"Very nice mentor", description:"I learnt a lot", rating:5, booking_id:1)
+# User.create(email:"giulia@gmail.com", password:"hellosoraya123", name:"giulia", bio:"Hello world", is_mentor:true)
+# User.create(email:"marco@notgmail.com", password:"hellonaelle123", name:"marco", bio:"Hello world", is_mentor:false)
+
+
+category = Category.create(name:"public")
+file_path = File.join(Rails.root, "/app/assets/images/category-public.jpg")
+category.image.attach(io: File.open(file_path), filename: "public.jpg")
+
+
+category = Category.create(name:"cooking")
+file_path = File.join(Rails.root, "/app/assets/images/category-cooking.jpg")
+category.image.attach(io: File.open(file_path), filename: "cooking.jpg")
+
+
+category = Category.create(name:"dancing")
+file_path = File.join(Rails.root, "/app/assets/images/category-dancing.jpg")
+category.image.attach(io: File.open(file_path), filename: "dancing.jpg")
+
+category = Category.create(name:"yoga")
+file_path = File.join(Rails.root, "/app/assets/images/category-yoga.jpg")
+category.image.attach(io: File.open(file_path), filename: "yoga.jpg")
+
+
+
+# Lesson.create(user_id:1, name:"speaking in public", description:"we will talk", duration:1, price:"40", category_id:1)
+
+# Booking.create(date: Date.today, lesson_id:1, user_id:2)
+
+# Review.create(title:"Very nice mentor", description:"I learnt a lot", rating:5, booking_id:1)
+
+
