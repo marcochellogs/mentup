8ba5cd08674ab41d61eed854b32b924b12123129
Giulia Martinelli <giulia.martinelli.uni@gmail.com>
2020-10-28 12:47:01 +0100

    implemented bookings#new and reviews#new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,11 +5,14 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @lesson = Lesson.find(params[:lesson_id])
     @booking = Booking.new
   end
 
   def create
+    @lesson = Lesson.find(params[:lesson_id])
     @booking = Booking.new(allowed_params)
+    @booking.lesson_id = params[:lesson_id]
     @booking.user = current_user
     if @booking.save
       redirect_to lesson_path(params[:lesson_id])
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,13 @@
 class ReviewsController < ApplicationController
   def new
     @review = Review.new
+    @booking = Booking.find(params[:booking_id])
   end
 
   def create
     @booking = Booking.find(params[:booking_id])
     @review = Review.new(allowed_params)
+    @review.booking_id = params[:booking_id]
     if @review.save
       redirect_to profile_path(@booking.user)
     else
@@ -17,6 +19,5 @@ class ReviewsController < ApplicationController
 
   def allowed_params
     params.require(:review).permit(:title, :description, :rating, :booking_id)
-
   end
 end
