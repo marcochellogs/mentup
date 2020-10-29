0b057fafd59c35857e56dba3bdc5974dae144068
Giulia Martinelli <giulia.martinelli.uni@gmail.com>
2020-10-29 13:36:16 +0100

    first try went deeply wrong

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,20 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require jquery.ui.all
+require jquery.ui.datepicker-en
+require jquery.ui.datepicker-es
+require jquery.ui.datepicker-fr
 
+$("input.datepicker").each(function(input) {
+  $(this).datepicker({
+    dateFormat: "yy-mm-dd",
+    altField: $(this).next()
+  })
+
+  // If you use i18n-js you can set the locale like that
+  $(this).datepicker("option", $.datepicker.regional[I18n.currentLocale()]);
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
